f81e7915dc737785601b85903d8beb11f42657db
Michael McAlynn <michael.mcalynn@hpe.com>
2016-03-14 18:34:36 +0000

    CAF-626 - Remove set storage reference from Document interface.

--- src/main/java/com/hpe/caf/messagebuilder/Document.java
+++ src/main/java/com/hpe/caf/messagebuilder/Document.java
@@ -4,6 +4,5 @@ package com.hpe.caf.messagebuilder;
  * Simple interface onto a document representation
  */
 public interface Document {
-    void setStorageReference(String storageReference);
     String getStorageReference();
 }
